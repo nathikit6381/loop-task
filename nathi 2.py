64aac465196faa9d4382f28a475f733e20fa7114
nathikit6381 <nathiyakala2014@gmail.com>
2026-03-18 13:42:32 +0530

    Add files via upload

---
+++ nathi 2.py
@@ -0,0 +1,50 @@
+#task=2
+#q=1
+"""num=int(input("enter the number"))
+even=0
+odd=0
+for num in range(1,num+1):
+  if num%2==0:
+    even+=num
+    print("even number",even)
+  else:
+    odd+=num
+    print("odd number",odd)"""
+#q=2
+
+'''a=int(input("enter the number"))
+for i in range(1,11):
+    print(a,"*",i,"=",a*i)'''
+   
+#q=3
+    
+'''a=int(input("enter the number"))
+fact=1
+for i in range(1,a+1):
+   fact=fact*i
+   print(fact)
+   '''
+#q=4
+'''a=65
+for i in range(1,8):
+    for j in range(i):
+        print(chr(a),end=' ')
+        a+=1
+        if a>90:
+           a=65
+    print( )
+    '''
+#q=5
+
+'''a=90
+for i in range(1,8):
+    for j in range (i):
+        print(chr(a),end=' ')
+        a-=1
+    if a>=90:
+        a=65
+    print( )'''
+
+     
+ 
+
